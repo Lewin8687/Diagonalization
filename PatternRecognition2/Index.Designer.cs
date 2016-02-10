16de0551f0317e33c74fbd06c7cb926fa86bcc44
Lewin8687 <lewin8687@gmail.com>
2016-02-10 03:32:56 -0500

    first commit

---
+++ PatternRecognition2/Index.Designer.cs
@@ -0,0 +1,365 @@
+﻿namespace PatternRecognition2
+{
+    partial class Diagonaliation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_start = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txb_m11 = new System.Windows.Forms.TextBox();
+            this.txb_m13 = new System.Windows.Forms.TextBox();
+            this.txb_m12 = new System.Windows.Forms.TextBox();
+            this.txb_m22 = new System.Windows.Forms.TextBox();
+            this.txb_m23 = new System.Windows.Forms.TextBox();
+            this.txb_m21 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txb_result = new System.Windows.Forms.TextBox();
+            this.txb_a = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txb_b = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txb_c = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txb_alpha = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pn_12before = new System.Windows.Forms.Panel();
+            this.pn_13before = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.pn_12after = new System.Windows.Forms.Panel();
+            this.pn_13after = new System.Windows.Forms.Panel();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_start
+            // 
+            this.btn_start.Location = new System.Drawing.Point(405, 36);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(75, 23);
+            this.btn_start.TabIndex = 0;
+            this.btn_start.Text = "Start";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "M1 = ";
+            // 
+            // txb_m11
+            // 
+            this.txb_m11.Location = new System.Drawing.Point(59, 18);
+            this.txb_m11.Name = "txb_m11";
+            this.txb_m11.Size = new System.Drawing.Size(21, 21);
+            this.txb_m11.TabIndex = 2;
+            this.txb_m11.Text = "4";
+            this.txb_m11.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txb_m13
+            // 
+            this.txb_m13.Location = new System.Drawing.Point(149, 18);
+            this.txb_m13.Name = "txb_m13";
+            this.txb_m13.Size = new System.Drawing.Size(24, 21);
+            this.txb_m13.TabIndex = 3;
+            this.txb_m13.Text = "4";
+            this.txb_m13.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txb_m12
+            // 
+            this.txb_m12.Location = new System.Drawing.Point(103, 18);
+            this.txb_m12.Name = "txb_m12";
+            this.txb_m12.Size = new System.Drawing.Size(21, 21);
+            this.txb_m12.TabIndex = 4;
+            this.txb_m12.Text = "2";
+            this.txb_m12.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txb_m22
+            // 
+            this.txb_m22.Location = new System.Drawing.Point(103, 45);
+            this.txb_m22.Name = "txb_m22";
+            this.txb_m22.Size = new System.Drawing.Size(21, 21);
+            this.txb_m22.TabIndex = 8;
+            this.txb_m22.Text = "2";
+            this.txb_m22.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txb_m23
+            // 
+            this.txb_m23.Location = new System.Drawing.Point(149, 45);
+            this.txb_m23.Name = "txb_m23";
+            this.txb_m23.Size = new System.Drawing.Size(24, 21);
+            this.txb_m23.TabIndex = 7;
+            this.txb_m23.Text = "-4";
+            this.txb_m23.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txb_m21
+            // 
+            this.txb_m21.Location = new System.Drawing.Point(59, 45);
+            this.txb_m21.Name = "txb_m21";
+            this.txb_m21.Size = new System.Drawing.Size(21, 21);
+            this.txb_m21.TabIndex = 6;
+            this.txb_m21.Text = "-4";
+            this.txb_m21.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "M2 = ";
+            // 
+            // txb_result
+            // 
+            this.txb_result.Location = new System.Drawing.Point(519, 38);
+            this.txb_result.Multiline = true;
+            this.txb_result.Name = "txb_result";
+            this.txb_result.Size = new System.Drawing.Size(159, 531);
+            this.txb_result.TabIndex = 9;
+            // 
+            // txb_a
+            // 
+            this.txb_a.Location = new System.Drawing.Point(236, 18);
+            this.txb_a.Name = "txb_a";
+            this.txb_a.Size = new System.Drawing.Size(21, 21);
+            this.txb_a.TabIndex = 11;
+            this.txb_a.Text = "2";
+            this.txb_a.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(210, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "a = ";
+            // 
+            // txb_b
+            // 
+            this.txb_b.Location = new System.Drawing.Point(291, 18);
+            this.txb_b.Name = "txb_b";
+            this.txb_b.Size = new System.Drawing.Size(21, 21);
+            this.txb_b.TabIndex = 13;
+            this.txb_b.Text = "3";
+            this.txb_b.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(266, 27);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "b = ";
+            // 
+            // txb_c
+            // 
+            this.txb_c.Location = new System.Drawing.Point(344, 18);
+            this.txb_c.Name = "txb_c";
+            this.txb_c.Size = new System.Drawing.Size(21, 21);
+            this.txb_c.TabIndex = 15;
+            this.txb_c.Text = "4";
+            this.txb_c.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(318, 27);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 12);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "c = ";
+            // 
+            // txb_alpha
+            // 
+            this.txb_alpha.Location = new System.Drawing.Point(292, 45);
+            this.txb_alpha.Name = "txb_alpha";
+            this.txb_alpha.Size = new System.Drawing.Size(26, 21);
+            this.txb_alpha.TabIndex = 17;
+            this.txb_alpha.Text = "0.1";
+            this.txb_alpha.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(242, 54);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 12);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "alpha = ";
+            // 
+            // pn_12before
+            // 
+            this.pn_12before.Location = new System.Drawing.Point(17, 97);
+            this.pn_12before.Name = "pn_12before";
+            this.pn_12before.Size = new System.Drawing.Size(240, 240);
+            this.pn_12before.TabIndex = 18;
+            // 
+            // pn_13before
+            // 
+            this.pn_13before.Location = new System.Drawing.Point(263, 97);
+            this.pn_13before.Name = "pn_13before";
+            this.pn_13before.Size = new System.Drawing.Size(240, 240);
+            this.pn_13before.TabIndex = 19;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(517, 14);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(47, 12);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "Output:";
+            // 
+            // pn_12after
+            // 
+            this.pn_12after.Location = new System.Drawing.Point(17, 365);
+            this.pn_12after.Name = "pn_12after";
+            this.pn_12after.Size = new System.Drawing.Size(240, 240);
+            this.pn_12after.TabIndex = 21;
+            // 
+            // pn_13after
+            // 
+            this.pn_13after.Location = new System.Drawing.Point(263, 365);
+            this.pn_13after.Name = "pn_13after";
+            this.pn_13after.Size = new System.Drawing.Size(240, 240);
+            this.pn_13after.TabIndex = 22;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(18, 82);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(95, 12);
+            this.label8.TabIndex = 23;
+            this.label8.Text = "x1-x2 (before):";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(261, 82);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(95, 12);
+            this.label9.TabIndex = 24;
+            this.label9.Text = "x1-x3 (before):";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(15, 350);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(89, 12);
+            this.label10.TabIndex = 25;
+            this.label10.Text = "x1-x2 (after):";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(261, 350);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(89, 12);
+            this.label11.TabIndex = 26;
+            this.label11.Text = "x1-x3 (after):";
+            // 
+            // Diagonaliation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(691, 608);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.pn_13after);
+            this.Controls.Add(this.pn_12after);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.pn_13before);
+            this.Controls.Add(this.pn_12before);
+            this.Controls.Add(this.txb_alpha);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txb_c);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txb_b);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txb_a);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txb_result);
+            this.Controls.Add(this.txb_m22);
+            this.Controls.Add(this.txb_m23);
+            this.Controls.Add(this.txb_m21);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txb_m12);
+            this.Controls.Add(this.txb_m13);
+            this.Controls.Add(this.txb_m11);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_start);
+            this.Name = "Diagonaliation";
+            this.Text = "Diagonaliation";
+            this.Load += new System.EventHandler(this.Index_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txb_m11;
+        private System.Windows.Forms.TextBox txb_m13;
+        private System.Windows.Forms.TextBox txb_m12;
+        private System.Windows.Forms.TextBox txb_m22;
+        private System.Windows.Forms.TextBox txb_m23;
+        private System.Windows.Forms.TextBox txb_m21;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txb_result;
+        private System.Windows.Forms.TextBox txb_a;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txb_b;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txb_c;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txb_alpha;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Panel pn_12before;
+        private System.Windows.Forms.Panel pn_13before;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Panel pn_12after;
+        private System.Windows.Forms.Panel pn_13after;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+    }
+}
---
+++ PatternRecognition2/Index.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Bluebit.MatrixLibrary;
+
+namespace PatternRecognition2
+{
+    public partial class Diagonaliation : Form
+    {
+        private double[] means1;
+        private double[] means2;
+        private double[,] matrix1;
+        private double[,] matrix2;
+        public Diagonaliation()
+        {
+            InitializeComponent();
+        }
+
+        private void Index_Load(object sender, EventArgs e)
+        {
+            var a = Convert.ToDouble(txb_a.Text);
+            var b = Convert.ToDouble(txb_b.Text);
+            var c = Convert.ToDouble(txb_c.Text);
+            var alpha = Convert.ToDouble(txb_alpha.Text);
+
+            matrix1 = new double[3, 3] { 
+                { a*a, alpha*a*b, alpha*a*c }, 
+                { alpha*a*b, b*b, alpha*b*c }, 
+                { alpha*a*c, alpha*b*c, c*c } };
+            matrix2 = new double[3, 3] { 
+                { c*c, alpha*b*c, alpha*a*c }, 
+                { alpha*b*c, b*b, alpha*a*b }, 
+                { alpha*a*c, alpha*a*b, a*a } };
+
+            means1 = new double[3] { 
+                Convert.ToInt32(txb_m11.Text), 
+                Convert.ToInt32(txb_m12.Text), 
+                Convert.ToInt32(txb_m13.Text) };
+            means2 = new double[3] { 
+                Convert.ToInt32(txb_m21.Text), 
+                Convert.ToInt32(txb_m22.Text), 
+                Convert.ToInt32(txb_m23.Text) };
+        }
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            Convertor();
+        }
+        public void Convertor()
+        {
+            var eigen1 = new Eigen(matrix1);
+            
+            var p1 = new Matrix();
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    p1[i, j] = eigen1.Eigenvectors[i, j].Real;
+                }
+            }
+            var y1 = Matrix.Multiply(Matrix.Multiply(p1.Inverse(), matrix1), p1);
+            var diagonalizing = new Matrix();
+            for (int i = 0; i < 3; i++)
+            {
+                diagonalizing[i, i] = 1 / Math.Sqrt(y1[i, i]);
+            }
+            var y2 = Matrix.Multiply(p1.Transpose(),Matrix.Multiply(matrix2,p1));
+            var z2 = Matrix.Multiply(Matrix.Multiply(diagonalizing.Transpose(), y2),diagonalizing);
+            
+            var eigen2 = new Eigen(z2);
+            var eigen_vectors2 = new Matrix();
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    eigen_vectors2[i, j] = eigen2.Eigenvectors[i, j].Real;
+                }
+            }
+            var v2 = Matrix.Multiply(Matrix.Multiply(eigen_vectors2.Transpose(),z2), eigen_vectors2);
+            var new_means1 = Matrix.Multiply(eigen_vectors2,Matrix.Multiply(diagonalizing, Matrix.Multiply(p1.Transpose(), means1)));
+            var new_means2 = Matrix.Multiply(eigen_vectors2,Matrix.Multiply(Matrix.Multiply(diagonalizing, p1.Transpose()), means2));
+            PrintMatrix(y1, "y1");
+            PrintMatrix(y2, "y2");
+            PrintMatrix(z2, "z2");
+            PrintMatrix(v2, "v2");
+
+            eigen2 = new Eigen(matrix2);
+            var p2 = new Matrix();
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    p2[i, j] = eigen2.Eigenvectors[i, j].Real;
+                }
+            }
+            var y = Matrix.Multiply(Matrix.Multiply(p2.Inverse(), matrix2), p2);
+            var diagonalizing2 = new Matrix();
+            for (int i = 0; i < 3; i++)
+            {
+                diagonalizing2[i, i] = Math.Sqrt(y[i, i]);
+            }
+            var diagonalizing3 = new Matrix();
+            for (int i = 0; i < 3; i++)
+            {
+                diagonalizing3[i, i] = Math.Sqrt(y1[i, i]);
+            }
+            for (int i = 0; i < 200; i++)
+            {
+                var penBlue = new Pen(Color.Blue);
+                var penRed = new Pen(Color.Red);
+                var vector1 = GenerateGaussianVector(means1);
+                vector1 = Matrix.Multiply(p1, Matrix.Multiply(diagonalizing3, vector1));
+                var vector2 = GenerateGaussianVector(means2);
+                vector2 = Matrix.Multiply(p2, Matrix.Multiply(diagonalizing2, vector2));
+                // x1-x2 before
+                var g = pn_12before.CreateGraphics();
+                var point = new Point((int)(vector1[0] * 6 + 150), (int)(vector1[1] * 6 + 130));
+                var s = new System.Drawing.Size(2,2);
+                var circle = new Rectangle(point,s);
+                g.DrawRectangle(penBlue, circle);
+
+                point = new Point((int)(vector2[0] * 6 + 150), (int)(vector2[1] * 6 + 130));
+                circle = new Rectangle(point, s);
+                g.DrawRectangle(penRed, circle);
+                // x1-x3 before
+                g = pn_13before.CreateGraphics();
+                point = new Point((int)(vector1[0] * 6 + 150), (int)(vector1[2] * 6 + 130));
+                circle = new Rectangle(point, s);
+                g.DrawRectangle(penBlue, circle);
+
+                point = new Point((int)(vector2[0] * 6 + 150), (int)(vector2[2] * 6 + 130));
+                circle = new Rectangle(point, s);
+                g.DrawRectangle(penRed, circle);
+                // after
+                vector1 = GenerateGaussianVector(new_means1);
+                vector2 = GenerateGaussianVector(new_means2);
+                var eigen = new Eigen(v2);
+                var m = new Matrix();
+                for (int k = 0; k < 3; k++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        m[k, j] = eigen.Eigenvectors[k, j].Real;
+                    }
+                }
+                var result1 = Matrix.Multiply(Matrix.Multiply(m.Inverse(), v2), m);
+                var diagonal = new Matrix();
+                for (int k = 0; k < 3; k++)
+                    diagonal[k, k] = 1 / Math.Sqrt(result1[k, k]);
+                vector2 = Matrix.Multiply(m, Matrix.Multiply(diagonal, vector2));
+                // x1-x2 after
+                g = pn_12after.CreateGraphics();
+                point = new Point((int)(vector1[0] * 15 + 110), (int)(vector1[1] * 15 + 110));
+                circle = new Rectangle(point, s);
+                g.DrawRectangle(penBlue, circle);
+                
+                point = new Point((int)(vector2[0] * 15 + 110), (int)(vector2[1] * 15 + 110));
+                circle = new Rectangle(point, s);
+                g.DrawRectangle(penRed, circle);
+                // x1-x3 after
+                g = pn_13after.CreateGraphics();
+                point = new Point((int)(vector1[0] * 15 + 110), (int)(vector1[2] * 15 + 110));
+                circle = new Rectangle(point, s);
+                g.DrawRectangle(penBlue, circle);
+
+                point = new Point((int)(vector2[0] * 15 + 110), (int)(vector2[2] * 15 + 110));
+                circle = new Rectangle(point, s);
+                g.DrawRectangle(penRed, circle);
+            }
+        }
+        public void PrintMatrix(Matrix matrix, string name)
+        {
+            txb_result.Text += name + ":\r\n";
+            txb_result.Text += matrix.ToString();
+            txb_result.Text += "\r\n";
+        }
+        private static Random rand = new Random();
+        public double[] GenerateGaussianVector(double[] mean)
+        {
+            var vector = new double[mean.Length];
+            for (int i = 0; i < mean.Length; i++)
+            {
+                double temp = 0;
+                for (int j = 0; j < 12; j++)
+                {
+                    int randomNumber = rand.Next(0, 100);
+                    temp = temp + (double)randomNumber / 100;
+                }
+                temp = temp - 6;
+                vector[i] = mean[i] + temp;
+            }
+            return vector;
+        }
+    }
+}
